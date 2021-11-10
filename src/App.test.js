b232e70215e906b2288d4c30f838aa45f098860f
Hojjat Jokar <7152729+hojjatjokar@users.noreply.github.com>
2021-11-11 00:23:37 +0330

    feat: add addition

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,53 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
   test('it renders', () => {
     render(<App />);
+  });
+
+  test('display value', () => {
+    render(<App />);
+
+    fireEvent.click(screen.getByText(/1/i));
+    expect(screen.getByTestId(/display/i).textContent).toBe('1');
+    fireEvent.click(screen.getByText(/2/i));
+    expect(screen.getByTestId(/display/i).textContent).toBe('12');
+  });
+
+  test('plus: 1+2 to be 3', () => {
+    render(<App />);
+
+    fireEvent.click(screen.getByText(/1/i));
+    expect(screen.getByTestId(/display/i).textContent).toBe('1');
+    fireEvent.click(screen.getByText(/\+/i));
+    expect(screen.getByTestId(/display/i).textContent).toBe('1');
+    fireEvent.click(screen.getByText(/2/i));
+    expect(screen.getByTestId(/display/i).textContent).toBe('2');
+    fireEvent.click(screen.getByText(/=/i));
+    expect(screen.getByTestId(/display/i).textContent).toBe('3');
+  });
+
+  test('plus then plus: 1 + 2 + 2', () => {
+    render(<App />);
+
+    fireEvent.click(screen.getByText(/1/i));
+    fireEvent.click(screen.getByText(/\+/i));
+    fireEvent.click(screen.getByText(/2/i));
+    fireEvent.click(screen.getByText(/=/i));
+    expect(screen.getByTestId(/display/i).textContent).toBe('3');
+    fireEvent.click(screen.getByText(/\+/i));
+    fireEvent.click(screen.getByText(/2/i));
+    fireEvent.click(screen.getByText(/=/i));
+    expect(screen.getByTestId(/display/i).textContent).toBe('5');
+  });
+
+  test('clear button', () => {
+    render(<App />);
+
+    fireEvent.click(screen.getByText(/1/i));
+    fireEvent.click(screen.getByText(/Clear/i));
+    expect(screen.getByTestId(/display/i).textContent).toBe('0');
   });
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Home from './App';
+import 'tailwindcss/tailwind.css';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
